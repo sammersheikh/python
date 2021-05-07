4e00239f940deef86eed4706bf6aa4bbfbd215e7
Sammer Sheikh <51171667+sammersheikh@users.noreply.github.com>
2021-05-06 22:54:04 -0700

    Add files via upload

---
+++ numeric.py
@@ -0,0 +1,16 @@
+num = 1
+
+num += 1 #equivalent to num = num + 1
+
+print(abs(-3)) #absolute value (get positive number)
+
+print(round(3.75, 1)) #round to nearest digit, second number means round to the first digit after the decimal
+
+num_1 = '100'
+
+num_2 = '200' #these are strings, not integers
+
+num_1 = int(num_1) #prefacing with int() casts the string to that type
+num_2 = int(num_2)
+
+print(num_1 + num_2)
