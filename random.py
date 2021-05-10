ea5a68a78b850d82722dc5a5543988a5840ad26e
Sammer Sheikh <51171667+sammersheikh@users.noreply.github.com>
2021-05-09 22:53:56 -0700

    Add files via upload

---
+++ random.py
@@ -0,0 +1,15 @@
+num_1 = 5
+
+num_2 = 4
+
+print(num_1 + num_2)
+
+print(num_1 - num_2)
+
+#---------------------------#
+
+family_members = ("Sammer", "Mel", "Mandy")
+
+print(family_members[0]) # 0 index is the first item on the lists
+
+print(family_members[0:2]) # will output the 0 index but will not output index 2, will stop right before 2
