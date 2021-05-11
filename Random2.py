b18ff7b7cd989aa8bc67670944d8b10c99272443
Sammer Sheikh <51171667+sammersheikh@users.noreply.github.com>
2021-05-10 22:35:49 -0700

    Add files via upload

---
+++ Random2.py
@@ -0,0 +1,30 @@
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
+popped = courses.pop() # can keep popping off values until list is empty
+
+print(popped)
+print(courses)
+
+
+#reverse method: shows the list in reverse
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
+courses.reverse()
+
+print(courses)
+
+
+#sort method: sorts list in alphabetical order
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
+nums = [1, 5, 2, 4, 3]
+
+courses.sort()
+nums.sort() # numbers are attended to
+
+print(courses)
+print(nums)
+
+print(courses[-1])
